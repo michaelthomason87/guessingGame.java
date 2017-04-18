e56322b9d2240bb17b4ce579f33194fa9fc67a5f
DeathCamel57 <DeathCamel57@gmail.com>
2017-04-18 13:14:59 -0400

    Shrinking code for master

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -3,62 +3,22 @@ public class GuessingGame
 {
 	public static void main (String [] args)
 	{
-
-		/*
-		 * Starting text...
-		 */
-
 		System.out.println("Choose a number between 1 and 100...");
 		System.out.println("Press any letter, then enter when you've chosen one.");
-
-		//Variables
-
-		/*
-		 * Random Number Generator not needed...
-		 * Random randomNumber = new Random();
-		 * int computerValue = randomNumber.nextInt(100);
-		 * 
-		 * Success variable not needed, as we'll just exit cleanly after correctly guessed...
-		 * int success = 0;
-		 */
 		Scanner kbd = new Scanner(System.in);
 		int numberOfTries = 0;
-
-		/*
-		 * Renamed "guess" to "response" 
-		 * 
-		 * Added "minThreshold" and "maxThreshold" variables
-		 * 
-		 * Description of "minThreshold" and "maxThreshold":
-		 * 		These variables hold the minimum and maximum numbers that the user can choose from
-		 * 		This is stored, and will change as the computer narrows the range the user's number could be in
-		 */
-
-		// Empty input char
 		char response = kbd.next().charAt(0);
-
-		// Threshold ints
 		int minThreshold = 1;
 		int maxThreshold = 100;
 		int midThreshold = 0;
-
-		//Logic and While Loop
-
-		while (true)	// Do this section until program exits
+		while (true)
 		{
-			/*
-			 * I don't believe that ints can store decimals, so I apply the modulus (%) to catch odd numbers
-			 * This is where the middle value (rounded up) is calculated...
-			 */
 			if ( (maxThreshold - minThreshold) % 2 == 1 ) {
-				// Odd numbers come this way
 				midThreshold = maxThreshold - ((maxThreshold - minThreshold + 1) / 2);
 			} else {
-				// Even numbers come this way...
 				midThreshold = maxThreshold - ((maxThreshold - minThreshold) / 2);
 			}
-			// This if doesn't have to exist, but it looks better if it asks if your number is higher one time, and lower another...
-			if (numberOfTries % 2 == 0)		// If "numberOfTries" is even...
+			if (numberOfTries % 2 == 0)
 			{
 				System.out.println("Is your number greater than or equal to " + midThreshold + "? (press y/n)");
 				response = kbd.next().charAt(0);
@@ -78,24 +38,12 @@ public class GuessingGame
 					minThreshold = midThreshold + 1;
 				}
 			}
-
 			numberOfTries++;
-
-			/*
-			 * Only do stuff below this on the last two guesses
-			 * 
-			 * Guess the bottom number
-			 */
-
 			if (maxThreshold - minThreshold <= 1) {
 				System.out.println("Is your number " + minThreshold + "? (press y/n)");
 				response = kbd.next().charAt(0);
 				if (response == 'y') {
 					System.out.println("It took me " + numberOfTries + " guesses");
-					/*
-					 * Tell the user good game...
-					 * And yes, I did HAVE to use humor somewhere
-					 */
 					System.out.println("gg m8 #rekt");
 					kbd.close();
 					System.exit(0);
