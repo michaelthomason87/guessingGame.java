9fdcebbce211997172099417ca57965d05a3e832
DeathCamel57 <DeathCamel57@gmail.com>
2017-04-18 13:22:25 -0400

    Shrinking codebase

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -3,48 +3,15 @@ public class GuessingGame
 {
 	public static void main (String [] args)
 	{
-
-		/*
-		 * Starting text...
-		 */
-
 		System.out.println("Choose a number between 1 and 100...");
 		System.out.println("Press any letter, then enter when you've chosen one.");
-
-		//Variables
-
-		/*
-		 * Random Number Generator not needed...
-		 * Random randomNumber = new Random();
-		 * int computerValue = randomNumber.nextInt(100);
-		 * 
-		 * Success variable not needed, as we'll just exit cleanly after correctly guessed...
-		 * int success = 0;
-		 */
 		Scanner kbd = new Scanner(System.in);
 		int numberOfTries = 0;
-
-		/*
-		 * Renamed "guess" to "response" 
-		 * 
-		 * Added "minThreshold" and "maxThreshold" variables
-		 * 
-		 * Description of "minThreshold" and "maxThreshold":
-		 * 		These variables hold the minimum and maximum numbers that the user can choose from
-		 * 		This is stored, and will change as the computer narrows the range the user's number could be in
-		 */
-
-		// Empty input char
 		char response = kbd.next().charAt(0);
-
-		// Threshold ints
 		int minThreshold = 1;
 		int maxThreshold = 100;
 		int midThreshold = 0;
-
-		//Logic and While Loop
-
-		while (true)	// Do this section until program exits
+		while (true)
 		{
 			if ( (maxThreshold - minThreshold) % 2 == 1 ) {
 				midThreshold = maxThreshold - ((maxThreshold - minThreshold + 1) / 2);
@@ -71,15 +38,7 @@ public class GuessingGame
 					minThreshold = midThreshold + 1;
 				}
 			}
-
 			numberOfTries++;
-
-			/*
-			 * Only do stuff below this on the last two guesses
-			 * 
-			 * Guess the bottom number
-			 */
-
 			if (maxThreshold - minThreshold <= 1) {
 				System.out.println("Is your number " + minThreshold + "? (press y/n)");
 				response = kbd.next().charAt(0);
